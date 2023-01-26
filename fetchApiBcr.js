da73aac98f8b9ea91f86739620e40b654579c080
Brayan Correa <brayan_seyer@hotmail.com>
2023-01-26 12:35:03 -0600

    feat:display with loading from api and localdata

--- fetchApiBcr.js
+++ fetchApiBcr.js
@@ -1,102 +1,117 @@
 let counter = 0;
-let fechaCaducidad=0;
+let fechaCaducidad = 0;
 let localData;
 
 const desarrolloAsíncrono = (delay) => {
     setTimeout(() => {
         console.log(`Asíncrono - Cargando Data: Tiempo de espera aproximado ${delay} miliegundos`)
         getUsers(1);
-       
+        btnDisplayList.disabled=false;
     }
-        , delay); 
+        , delay);
 }
 
-const displayInfoFromLocalStorage = () => {
-
-    localData = JSON.parse(localStorage.getItem("usersData"))
-    for (let user of localData.data) {
-
-        console.log(`${user.id}-${user.first_name}-${user.last_name}`);
-
-        const divMainCol = document.createElement("div");
-        divMainCol.classList.add("col-lg-4","col-md-6","col-sm-12","d-flex", "justify-content-center","my-3");
-        divMainCol.id = `divMainCol${counter}`
-        document.getElementById("rowMainContainer").appendChild(divMainCol)
-
-        const divCol = document.createElement("div");
-        divCol.classList.add("card","align-items-center");
-        divCol.style.width = "18rem";
-        divCol.id = `divCol${counter}`
-        document.getElementById(`divMainCol${counter}`).appendChild(divCol)
-        console.log("niño implantado");
-
-        const imgDiv = document.createElement("img");
-        imgDiv.classList.add("card-img-top","rounded-circle");
-        imgDiv.src = `${user.avatar}`;
-        document.getElementById(`divCol${counter}`).appendChild(imgDiv)
-
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        cardBody.id = `cardBody${counter}`;
-        document.getElementById(`divCol${counter}`).appendChild(cardBody)
-
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.innerText = `${user.first_name}`;
-        document.getElementById(`cardBody${counter}`).appendChild(cardTitle)
-
-        const cardSubtitle = document.createElement("h6");
-        cardSubtitle.classList.add("card-subtitle");
-        cardSubtitle.innerText = user.last_name;
-        document.getElementById(`cardBody${counter}`).appendChild(cardSubtitle)
-
-        const email = document.createElement("p");
-        email.classList.add("card-text");
-        email.innerText = `${user.email}`;
-        document.getElementById(`cardBody${counter}`).appendChild(email)
-        counter++;
-    } 
+const displayInfoFromLocalStorage = (page) => {
     
+        localData = JSON.parse(localStorage.getItem(`usersData${page}`))
+       
+        for (let user of localData.data) {
+
+            console.log(`${user.id}-${user.first_name}-${user.last_name}`);
+
+            const divMainCol = document.createElement("div");
+            divMainCol.classList.add("col-lg-4", "col-md-6", "col-sm-12", "d-flex", "justify-content-center", "my-3");
+            divMainCol.id = `divMainCol${counter}`
+            document.getElementById("rowMainContainer").appendChild(divMainCol)
+
+            const divCol = document.createElement("div");
+            divCol.classList.add("card", "align-items-center");
+            divCol.style.width = "18rem";
+            divCol.id = `divCol${counter}`
+            document.getElementById(`divMainCol${counter}`).appendChild(divCol)
+            console.log("niño implantado");
+
+            const imgDiv = document.createElement("img");
+            imgDiv.classList.add("card-img-top", "rounded-circle");
+            imgDiv.src = `${user.avatar}`;
+            document.getElementById(`divCol${counter}`).appendChild(imgDiv)
+
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+            cardBody.id = `cardBody${counter}`;
+            document.getElementById(`divCol${counter}`).appendChild(cardBody)
+
+            const cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title");
+            cardTitle.innerText = `${user.id}.-${user.first_name}`;
+            document.getElementById(`cardBody${counter}`).appendChild(cardTitle)
+
+            const cardSubtitle = document.createElement("h6");
+            cardSubtitle.classList.add("card-subtitle");
+            cardSubtitle.innerText = user.last_name;
+            document.getElementById(`cardBody${counter}`).appendChild(cardSubtitle)
+
+            const email = document.createElement("p");
+            email.classList.add("card-text");
+            email.innerText = `${user.email}`;
+            document.getElementById(`cardBody${counter}`).appendChild(email)
+            counter++;
+
+        }
+
 }
 
-const getUsers = (page = 1) => {
+const getUsers = async (page = 1) => {
 
-    const url = `https://reqres.in/api/users?page=${page}`;
+    while (page <= 2) {
 
-    fetch(url)
-        .then(response => {
-            console.log("Status" + response.status);
-            return response.json();
-        })
+        const url = `https://reqres.in/api/users?page=${page}`;
 
-        .then(data => {
-            console.log(data);
-            localStorage.setItem("usersData",JSON.stringify(data));
-            localStorage.setItem("fechaCaducidad", (new Date().getTime()) + 60_000 );
-            document.getElementById("loader").style.display = "none";
-            displayInfoFromLocalStorage();
-        })
-        .catch(error => console.log(error));
+        await fetch(url)
+            .then(response => {
+                console.log("Status" + response.status);
+                return response.json();
+            })
+
+            .then(data => {
+                console.log(data.data);
+                dataArray = [];
+                dataArray.push(data.data);
+                console.log(dataArray);
+                localStorage.setItem(`usersData${page}`, JSON.stringify(data));
+                localStorage.setItem("fechaCaducidad", (new Date().getTime()) + 60_000);
+                document.getElementById("loader").style.display = "none";/*  */
+                displayInfoFromLocalStorage(page);
+            })
+            .catch(error => console.log(error));
+        page++;
+    }
+
 }
- 
+
 const btnDisplayList = document.getElementById("btn-primary");
 
 btnDisplayList.addEventListener("click", () => {
 
-let fechaActual = new Date().getTime() 
-console.log(fechaActual);
+    let fechaActual = new Date().getTime()
+    console.log(fechaActual);
     fechaCaducidad = localStorage.getItem('fechaCaducidad');
-    if(fechaCaducidad == null || fechaActual > fechaCaducidad){
+    if (fechaCaducidad == null || fechaActual > fechaCaducidad) {
         document.getElementById("greyBackground").style.display = "inline";
         document.getElementById("loader").style.display = "flex";
-        desarrolloAsíncrono(4000); 
+        btnDisplayList.disabled=true;
+        desarrolloAsíncrono(4000);
     }
 
     else {
-
-        displayInfoFromLocalStorage();
+        page=1;
+        do {
+            displayInfoFromLocalStorage(page);
+        page++;
+        console.log(page);
+    } while (page<=2);
     }
 
-  
+
 }
 )
